65b618270eb3e056d817498a58e83e9b02793e22
Math2121 <matheusdepaula527@gmail.com>
2021-06-08 21:56:52 -0300

    add Alpine Js

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -52,6 +52,8 @@
 
     @yield('content')
     @livewireScripts
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+
 </body>
 
 </html>
--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,6 +1,23 @@
-<div class="relative mt-3 md:mt-0">
-    <input wire:model.debounce.500ms="search" type="search" name="search" id="search" placeholder="Search"
-        class="bg-gray-800  text-sm rounded-full w-64 px-4 pl-8 py-1">
+<div class="relative mt-3 md:mt-0" x-data="{isOpen:true}" @click.away="isOpen = false">
+    <input
+    wire:model.debounce.500ms="search"
+    type="search"
+    name="search"
+    id="search"
+    placeholder="Search"
+    class="bg-gray-800  text-sm rounded-full w-64 px-4 pl-8 py-1"
+    @focus="isOpen = true"
+    x-ref="search"
+    @keydown.window="
+    if(event.keyCode === 191){
+     event.preventDefault();
+     $refs.search.focus();
+
+    }"
+    @keydown = "isOpen = true"
+    @keydown.escape.window = "isOpen = false"
+    @keydown.escape.window = "isOpen = false"
+    >
 
     <div class="absolute top-0">
         <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24">
@@ -11,13 +28,14 @@
     <div wire:loading class="spinner top-0 right-0 mr-4 mt-3"></div>
     @if (strlen($search) >= 2)
 
-        <div class="absolute bg-gray-800 rounded text-sm w-64 mt-4">
+        <div class="z-50 absolute bg-gray-800 rounded text-sm w-64 mt-4" x-show.transition.opacity="isOpen">
             @if ($searchResults->count() > 0)
                 <ul>
                     @foreach ($searchResults as $result)
                         <li class="border-b border-gray-700">
                             <a href="{{ route('movies.show', $result['id']) }}"
-                                class="block hover:bg-gray-700 px-3 py-3 flex items-center">
+                                class="block hover:bg-gray-700 px-3 py-3 flex items-center transition ease-in-out duration-150" @if ($loop->last)
+                                @keydown.tab="isOpen = false"@endif>
                                 @if ($result['poster_path'])
                                     <img src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}" alt=""
                                         class="w-8">
